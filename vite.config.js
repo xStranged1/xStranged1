a18de0161053af13988381fe2c45e117997db157
xStranged1 <fede.valle04@gmail.com>
2024-05-22 13:33:58 -0300

    fix: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://xStranged1.github.io/xstranged',
+  base:'https://xStranged1.github.io/xStranged1',
 })
